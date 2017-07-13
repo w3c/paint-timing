4226833fc3d1133cf358e1254580ecc1feddf6ea
plehegar <plh@w3.org>
2017-07-13 14:17:58 -0400

    Added images

--- compile.sh
+++ compile.sh
@@ -4,7 +4,9 @@ set -e # Exit with nonzero exit code if anything fails
 bikeshed spec
 
 if [ -d out ]; then
-  echo Copy painttiming.html into out/index.html
+  echo Copy the generated spec into out/index.html
   cp painttiming.html out/index.html
-fi
 
+  echo Copying images into out/
+  cp filmstrip.svg filmstrip.png out/
+fi
