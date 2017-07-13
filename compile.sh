d6e80cd63af02cf3a4b02794c5ca05ba42e87314
plehegar <plh@w3.org>
2017-07-13 12:08:19 -0400

    bikespec generates painttiming.html

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ set -e # Exit with nonzero exit code if anything fails
 bikeshed spec
 
 if [ -d out ]; then
-  echo Copy index.html into out/index.html
-  cp index.html out/index.html
+  echo Copy painttiming.html into out/index.html
+  cp painttiming.html out/index.html
 fi
 
